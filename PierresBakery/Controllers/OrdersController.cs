961ddbebe5dd41696fa566706670b363cddc64f8
Zoe Weinstein <mxzweinstein@gmail.com>
2022-03-05 19:00:13 -0700

    fixed orders pages

--- PierresBakery/Controllers/OrdersController.cs
+++ PierresBakery/Controllers/OrdersController.cs
@@ -11,21 +11,25 @@ namespace PierresBakery.Controllers
     [HttpGet("/vendors/{id}/orders/")]
     public ActionResult Index()
     {
-      return View();
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
     }
+    
 
     [HttpGet("/vendors/{id}/orders/new")]
-    public ActionResult New()
+    public ActionResult New(int vendorID)
     {
-      return View();
+      Vendor vendor = Vendor.Find(vendorID +1);
+      return View(vendor);
     }
-
+    /*
     [HttpPost("/vendors/{id}/orders/")]
     public ActionResult Create(string orderName, string orderDescription, int purchasePrice)
     {
       Order newOrder = new Order(orderName, orderDescription, purchasePrice);
       return RedirectToAction("Index");
     }
+    */
 
     
   }
